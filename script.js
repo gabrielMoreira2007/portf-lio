c4fd58417ad2b9cf1586e2b661c3bee0078534ad
gabrielMoreira2007 <gabrielmgseg@gmail.com>
2025-09-30 09:58:34 -0300

    commitz

---
+++ script.js
@@ -0,0 +1,17 @@
+// Navegação suave
+document.addEventListener("DOMContentLoaded", () => {
+  const links = document.querySelectorAll("a[href^='#']");
+
+  links.forEach(link => {
+    link.addEventListener("click", function(e) {
+      e.preventDefault();
+      const target = document.querySelector(this.getAttribute("href"));
+      if (target) {
+        window.scrollTo({
+          top: target.offsetTop - 60,
+          behavior: "smooth"
+        });
+      }
+    });
+  });
+});
